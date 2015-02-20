41c26af1f49d5ac54dcec9352ae460f8cfc41269
Robert Farrelly <hello@robertfarrelly.com>
2015-02-20 19:56:46 +0000

    working on slider for banner

--- js/main.js
+++ js/main.js
@@ -42,17 +42,38 @@ $(".down").click(function(){
 });
 
 
+// On load effects
 $(window).on('load',function(){
 	fadeInElements('.inner', 200, 500);
 	fadeInElements('.down', 600, 500);
+	replaceImgWithBgImg('.slide > img'); 
+
 });
 
-// setTimeout(function(){ $('.inner').addClass('show'); }, 500);
+$(window).on('resize', function(){
 
+	log('working');
+
+	var winHeight = $(window).height();
+
+	// $('#slides').css({
+	// 	height: winHeight
+	// });
+
+	$('.slidesjs-container').css({
+		height: winHeight
+		
+	});
+
+	// $('.sliderBg').css({
+	// 	height: winHeight
+	// });
+
+});
+
+// Fade in children of inner
 function fadeInElements(container, delay, duration) {
-
 	var elements = $(container).children();
-
 	(function theLoop (i) {
 		setTimeout(function () {
 			if (i < elements.length) {
@@ -64,4 +85,66 @@ function fadeInElements(container, delay, duration) {
 		}, delay);
 	})(0);
 }  
+
+
+
+// Slide Show function
+$(function() {
+  $('#slides').slidesjs({
+     navigation: {
+          effect: "fade"
+        },
+        pagination: {
+          effect: "fade"
+        },
+        effect: {
+          fade: {
+            speed: 500
+          }
+        },
+         callback: {
+          start: function(number) {
+              $('h2').fadeOut();
+          },
+          complete: function(number) {
+          // Use your browser console to view log
+          console.log('SlidesJS: Animation Complete. Current slide is #' + number);
+          // Change slide number on animation complete
+          $('#slidesjs-log .slidesjs-slide-number').text(number);
+          $('h2').fadeIn();
+      }
+    }
+  });
+});
+
+
+function replaceImgWithBgImg(elementSelector) {
+  
+  log('sound');
+
+  var winHeight = $(window).height();
+
+  $(elementSelector).each(function(i, elem) {
+
+    var img = $(elem);
+    div = $("<div />").css({
+    "background-image": "url(" + img.attr("src") + ")",
+    "background-repate": "no-repeat",
+    "background-size": "cover",
+    "height": winHeight,
+    "background-position": "center",
+    "width" : '100%' 
+  });
+      
+  div.html();
+  div.addClass('sliderBg');
+  img.replaceWith(div);
+
+});
+  
+}
+
+
+
+
 
